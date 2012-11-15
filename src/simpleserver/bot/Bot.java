472968a1db8a011226b0d7464914cc9686b6f931
Elviss Kuštans <ek@latvisoft.com>
2012-11-15 22:26:09 +0200

    Bot update for 1.4.4

--- src/simpleserver/bot/Bot.java
+++ src/simpleserver/bot/Bot.java
@@ -525,8 +525,8 @@ public class Bot {
       case (byte) 0x83: // Item Data
         in.readShort();
         in.readShort();
-        byte length = in.readByte();
-        readNBytes(0xff & length);
+        short length = in.readShort();
+        readNBytes(length);
         break;
       case (byte) 0x84: // added in 12w06a
         in.readInt();
